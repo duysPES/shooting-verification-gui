0d7102203dc04dd90d041d029dea53b390e67800
duysqubix <duys@qubixat.com>
2020-01-10 09:52:59 -0700

    added logic for communication with lisc

--- rusty-lisc/src/lib.rs
+++ rusty-lisc/src/lib.rs
@@ -1,5 +1,21 @@
 use bytes::BytesMut;
+use lazy_static::lazy_static;
+use serialport::prelude::*;
+use std::collections::HashMap;
+use std::io;
+use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+lazy_static! {
+    static ref LISC_CMDS: HashMap<&'static str, BytesMut> = {
+        let mut lisc_cmds = HashMap::new();
+        lisc_cmds.insert("zl", BytesMut::from("zl".as_bytes()));
+        lisc_cmds.insert("zL", BytesMut::from("zL".as_bytes()));
+        lisc_cmds
+    };
+}
 
 #[derive(Debug)]
 pub enum ConnMode {
@@ -23,18 +39,18 @@ pub enum Message {
 
 pub struct ConnPackage;
 
-impl ConnPackage {
-    pub fn create(infotype: InfoType, mode: ConnMode, msg: Message) -> () {
+impl<'a> ConnPackage {
+    pub fn create(infotype: InfoType, mode: ConnMode, msg: Message) -> BytesMut {
         let package = format!("{:?},{:?}", infotype, mode);
 
         match msg {
             Message::String(msg) => {
                 let package = format!("{},{}", package, msg);
-                println!("{}", package);
+                BytesMut::from(package.as_str())
             }
             Message::Switch((pos, addr)) => {
                 let package = format!("{},{:?}", pos, BytesMut::from(&addr[..]));
-                println!("{}", package);
+                BytesMut::from(package.as_str())
             }
             Message::Status((pos, addr, status)) => {
                 let package = format!(
@@ -43,8 +59,74 @@ impl ConnPackage {
                     BytesMut::from(&addr[..]),
                     BytesMut::from(&status[..])
                 );
-                println!("{}", package);
+                BytesMut::from(package.as_str())
             }
         }
+    }
+}
+
+pub struct Socket {
+    pub stream: TcpStream,
+}
+
+impl Socket {
+    pub fn new(stream: TcpStream) -> Self {
+        Self { stream: stream }
+    }
+
+    pub fn get_utf8(&mut self) -> io::Result<String> {
+        let mut buf = [0 as u8; 50];
+
+        if let Ok(n) = self.stream.read(&mut buf) {
+            if n < 2 {
+                return Ok("".to_string());
+            }
+            let resp = String::from_utf8(Vec::from(&buf[0..n - 2])).unwrap_or("".to_string());
+            Ok(resp)
+        } else {
+            Ok("".to_string())
+        }
+    }
+
+    pub fn write_with_delay(&mut self, buf: &[u8], delay: Duration) -> io::Result<()> {
+        self.stream.write(buf)?;
+        thread::sleep(delay);
+        Ok(())
+    }
+}
+
+pub struct Lisc {
+    pub serial: Box<dyn SerialPort>,
+}
+
+impl Lisc {
+    pub fn new() -> serialport::Result<Self> {
+        let serial = serialport::open("/dev/ttyS7")?;
+
+        Ok(Self { serial: serial })
+    }
+
+    pub fn listen(&mut self) -> serialport::Result<BytesMut> {
+        let mut buf: Vec<u8> = Vec::with_capacity(64);
+        let n = self.serial.read(buf.as_mut_slice())?;
+
+        Ok(BytesMut::from(&buf[0..n]))
+    }
+
+    pub fn write(&mut self, data: &BytesMut) -> serialport::Result<()> {
+        self.serial.write(&data[..])?;
+        Ok(())
+    }
+
+    pub fn write_with_delay(&mut self, data: &BytesMut, with_delay: u64) -> serialport::Result<()> {
+        self.serial.write(&data[..])?;
+        thread::sleep(Duration::from_millis(with_delay));
+        Ok(())
+    }
+
+    pub fn reset(&mut self) -> serialport::Result<()> {
+        self.write_with_delay(LISC_CMDS.get(&"zl").unwrap(), 100)?;
+        self.write_with_delay(LISC_CMDS.get(&"zL").unwrap(), 100)?;
+        Ok(())
     }
 }
--- rusty-lisc/src/main.rs
+++ rusty-lisc/src/main.rs
@@ -1,70 +1,67 @@
 mod lib;
-use bytes::BytesMut;
-use lib::{ConnMode, ConnPackage, InfoType, Message};
+use lib::{ConnMode, ConnPackage, InfoType, Lisc, Message, Socket};
 use std::error::Error;
 use std::io;
-use std::io::{Read, Write};
-use std::net::{Shutdown, TcpListener, TcpStream};
+use std::io::Write;
+use std::net::{Shutdown, TcpListener};
 use std::thread;
-
+use std::time::Duration;
 static LISC_ADDR: &'static str = "127.0.0.1:8001";
 
-fn get_utf8(stream: &mut TcpStream) -> io::Result<String> {
-    let mut buf = [0 as u8; 50];
+fn main() -> serialport::Result<()> {
+    let lisc = TcpListener::bind(LISC_ADDR)?;
+    println!("LISC server listening on {}", LISC_ADDR);
 
-    if let Ok(n) = stream.read(&mut buf) {
-        if n < 2 {
-            return Ok("".to_string());
+    for incoming in lisc.incoming() {
+        match incoming {
+            Ok(mut stream) => {
+                let s = stream.try_clone()?;
+                let mut socket = Socket::new(s);
+                let resp = socket.get_utf8()?;
+
+                let split_string: Vec<&str> = resp.split(",").collect();
+                println!("{:?}", split_string);
+
+                if let Some(start) = split_string.first() {
+                    match start {
+                        &"START" => {
+                            stream.write(b"OK")?;
+                            let _ = thread::spawn(move || {
+                                inventory(socket).unwrap_or_else(|e| {
+                                    println!("Inventory errored out with error={:?}", e);
+                                });
+                            });
+                        }
+                        _ => (),
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
         }
-        let resp = String::from_utf8(Vec::from(&buf[0..n - 2])).unwrap_or("".to_string());
-        Ok(resp)
-    } else {
-        Ok("".to_string())
     }
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let mode = ConnMode::Debug;
-    let info = InfoType::Other;
-    let msg = Message::Status((0, [1, 2, 3], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-    let package = ConnPackage::create(info, mode, msg);
-    // let lisc = TcpListener::bind(LISC_ADDR)?;
-
-    // println!("LISC server listening on {}", LISC_ADDR);
-
-    // for incoming in lisc.incoming() {
-    //     match incoming {
-    //         Ok(mut stream) => {
-    //             let resp = get_utf8(&mut stream)?;
-
-    //             let split_string: Vec<&str> = resp.split(",").collect();
-    //             println!("{:?}", split_string);
-
-    //             if let Some(start) = split_string.first() {
-    //                 match start {
-    //                     &"START" => {
-    //                         stream.write(b"OK")?;
-    //                         let _ = thread::spawn(move || {
-    //                             inventory(stream).unwrap_or_else(|e| {
-    //                                 println!("Inventory errored out with error={:?}", e);
-    //                             });
-    //                         });
-    //                     }
-    //                     _ => (),
-    //                 }
-    //             }
-    //         }
-    //         Err(e) => {
-    //             println!("Error: {}", e);
-    //         }
-    //     }
-    // }
     Ok(())
 }
 
-pub fn inventory(mut socket: TcpStream) -> std::io::Result<()> {
-    println!("Starting inventory process piping too: {:?}", socket);
-    let done = "DONE".as_bytes();
-    socket.write(done)?;
-    Ok(())
+pub fn inventory(socket: Socket) -> serialport::Result<()> {
+    let lisc = Lisc::new();
+
+    match lisc {
+        Ok(mut lisc) => {
+            // lisc has successfully connected! Now do the actual inventory!
+            lisc.reset()?;
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
+
+    // let package = ConnPackage::create(
+    //     InfoType::Other,
+    //     ConnMode::Main,
+    //     Message::String("Switch is not okay!".to_string()),
+    // );
+    // socket.write_with_delay(&package[..], Duration::from_millis(10))?;
+    // socket.stream.write(b"DONE")?;
+    // Ok(())
 }
